440ac0d2e9b82dc11af3cc919d51b35ee873ccdf
amit <[amirana14325@gmail.com]>
2021-02-11 15:04:16 +0530

    Added:: store daily wage into the arry

--- employeDetails.sh
+++ employeDetails.sh
@@ -4,21 +4,10 @@ EMP_RATE_PER_HR=20
 DAYS_IN_MONTH=20
 MAX_HRS_IN_MONTH=100
 
-empCheck=$((RANDOM%3))
 isPresentFullTime=1
 isPresentPartTime=2
-empMonthWage=0
 days=0
 hours=0
-
-while [[ $days -ne 20 && $hours -le 100 ]]
-do
-		((days++))
-		empCheck=$((RANDOM%3))
-		empHr="$( getWorkHr $empCheck)"
-		hours=$(($hours+$empHr))
-
-done
 
 function getWorkHr() {
 		local empCheck=$1
@@ -34,5 +23,22 @@ function getWorkHr() {
 		fi
 		echo $empHr
 }
+
+function calDailyWage() {
+		local empHr=$1
+		wage=$(($empHr*$EMP_RATE_PER_HR))
+		echo $wage
+}
+
+while [[ $days -ne $DAYS_IN_MONTH && $hours -le $MAX_HRS_IN_MONTH ]]
+do
+		((days++))
+		empCheck=$((RANDOM%3))
+		empHr="$( getWorkHr $empCheck)"
+		hours=$(($hours+$empHr))
+		dailyWage[$days]=$( calDailyWage $empHr )
+done
+
 empMonthWage=$(($hours*$EMP_RATE_PER_HR))
-echo $empMonthWage
+echo "Total Wage :"$empMonthWage
+echo "Daily Wage : "${dailyWage[@]}

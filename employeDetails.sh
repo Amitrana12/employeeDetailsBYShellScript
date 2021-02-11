33d48b97c9481920d2d66b6139f5df80c6ba6a65
amit <[amirana14325@gmail.com]>
2021-02-11 14:32:40 +0530

    Added:: using function for hour claculate for wage

--- employeDetails.sh
+++ employeDetails.sh
@@ -2,6 +2,7 @@
 #constant
 EMP_RATE_PER_HR=20
 DAYS_IN_MONTH=20
+MAX_HRS_IN_MONTH=100
 
 empCheck=$((RANDOM%3))
 isPresentFullTime=1
@@ -12,7 +13,16 @@ hours=0
 
 while [[ $days -ne 20 && $hours -le 100 ]]
 do
+		((days++))
 		empCheck=$((RANDOM%3))
+		empHr="$( getWorkHr $empCheck)"
+		hours=$(($hours+$empHr))
+
+done
+
+function getWorkHr() {
+		local empCheck=$1
+
 		if [ $empCheck -eq $isPresentFullTime ]
 		then
 				empHr=8
@@ -22,10 +32,7 @@ do
 		else
 				empHr=0
 		fi
-		hours=$(($hours+$empHr))
-		((days++))
-
-		empMonthWage=$(($empMonthWage+$empHr*$EMP_RATE_PER_HR))
-done
-
+		echo $empHr
+}
+empMonthWage=$(($hours*$EMP_RATE_PER_HR))
 echo $empMonthWage

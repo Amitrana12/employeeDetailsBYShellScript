72a81cbc47e76d6f52bd140ae340303d8914135e
amit <[amirana14325@gmail.com]>
2021-02-11 13:40:32 +0530

    Added:: employee Attandnce

--- employeDetails.sh
+++ employeDetails.sh
@@ -1,1 +1,13 @@
-#welcom to employe details
+#!/bin/bash -x
+
+empCheck=$((RANDOM%2))
+isPresent=1
+
+case $empCheck in
+	$isPresent)
+		echo Employee is Present
+		;;
+	*)
+		echo Employee is Absent
+		;;
+esac

62266fa52b5f896d19c2a6fbc34dea96384f8abf
amit <[amirana14325@gmail.com]>
2021-02-11 15:18:20 +0530

    Adding::littel update

--- employeDetails.sh
+++ employeDetails.sh
@@ -1,25 +1,14 @@
 #!/bin/bash -x
+
 #constant
 EMP_RATE_PER_HR=20
 DAYS_IN_MONTH=20
 MAX_HRS_IN_MONTH=100
 
-empCheck=$((RANDOM%3))
 isPresentFullTime=1
 isPresentPartTime=2
-empMonthWage=0
-days=0
+ays=0
 hours=0
-
-while [[ $days -ne 20 && $hours -le 100 ]]
-do
-		((days++))
-		empCheck=$((RANDOM%3))
-		empHr="$( getWorkHr $empCheck)"
-		hours=$(($hours+$empHr))
-
-done
-
 function getWorkHr() {
 		local empCheck=$1
 
@@ -34,5 +23,13 @@ function getWorkHr() {
 		fi
 		echo $empHr
 }
+while [[ $days -ne $DAYS_IN_MONTH && $hours -le $MAX_HRS_IN_MONTH ]]
+do
+		((days++))
+		empCheck=$((RANDOM%3))
+		empHr="$( getWorkHr $empCheck)"
+		hours=$(($hours+$empHr))
+
+done
 empMonthWage=$(($hours*$EMP_RATE_PER_HR))
 echo $empMonthWage
